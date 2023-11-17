b04b6651bbfce4a594f4c53c12f64277982d659b
Rob Brennan <rob@therobbrennan.com>
2023-11-17 14:46:14 -0800

    100% coverage achieved for the CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 import src.cli
+import runpy
 
 class TestCLI(unittest.TestCase):
     def test_game_flow(self):
@@ -22,6 +23,18 @@ class TestCLI(unittest.TestCase):
                     self.assertIn(expected_output, output)
                 # Check the game prompts for inputs
                 self.assertEqual(output.count("Choose rock, paper, or scissors (or 'q' to quit): "), 5)
+
+    def test_main_execution(self):
+        # Prepare to capture output
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            # Mock inputs to exit immediately
+            with patch('builtins.input', return_value='q'):
+                # Execute the CLI script as if it were being run directly
+                runpy.run_module('src.cli', run_name="__main__")
+
+                output = mock_stdout.getvalue()
+                # Assert that the script executed (e.g., by checking for the welcome message)
+                self.assertIn("Welcome to Rock, Paper, Scissors!", output)
 
 if __name__ == '__main__':
     unittest.main()
