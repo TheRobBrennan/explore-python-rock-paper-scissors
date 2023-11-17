832e4117a31cee3ff8b52a960384ba8a4227bb62
Rob Brennan <rob@therobbrennan.com>
2023-11-17 13:39:40 -0800

    Add tests for 100% coverage of RockPaperScissors

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 from src.game import RockPaperScissors
 
 class TestRockPaperScissors(unittest.TestCase):
@@ -22,6 +23,15 @@ class TestRockPaperScissors(unittest.TestCase):
         self.assertEqual(self.game.play('rock', 'paper'), "Computer wins")
         self.assertEqual(self.game.play('paper', 'scissors'), "Computer wins")
         self.assertEqual(self.game.play('scissors', 'rock'), "Computer wins")
+
+    @patch('random.choice')
+    def test_get_computer_choice(self, mock_choice):
+        mock_choice.return_value = 'rock'
+        self.assertEqual(self.game.get_computer_choice(), 'rock')
+        mock_choice.return_value = 'paper'
+        self.assertEqual(self.game.get_computer_choice(), 'paper')
+        mock_choice.return_value = 'scissors'
+        self.assertEqual(self.game.get_computer_choice(), 'scissors')
 
 if __name__ == '__main__':
     unittest.main()
